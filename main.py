8f073a0e03e5f852f74be5a280a7e768914a1fe4
Nakul1048 <nakulkochar@yahoo.com>
2021-12-21 19:28:00 -0500

    second

---
+++ main.py
@@ -0,0 +1,16 @@
+# name = input('What is your name? ')
+# print ('cool')
+# age = int(input('what is your age? '))
+# height = float(input('How tall are you in cm? '))
+#
+# print('Hello ' + name)
+# print('So you are ' + str(age)+ ' years old?')
+# print('wow You are ' + str(height) + ' cm tall!')
+
+
+print('checking to see if stephen is wack')
+wack = input('is stephen wack Y or N: ')
+if wack == 'Y':
+    print ("stephen is ")
+else:
+    print ('You are wrong, Stephen is wack')
